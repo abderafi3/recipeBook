b75eb7aefcdb2827bc05e67a057c76b4f85af007
abderafi3 <abderafi3@gmail.com>
2024-01-10 15:49:31 +0100

    update

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -7,9 +7,11 @@ import { Recipe } from '../../recipe.model';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent   {
-
   @Input() recipe! : Recipe;
+  recipeSelected = new EventEmitter<void>();
   
-
+onSelected(){
+this.recipeSelected.emit();
+}
 
 }
